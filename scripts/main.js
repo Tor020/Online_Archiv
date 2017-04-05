72ba39f939b85908bd03f4136b928f04d84e71e8
Kevin <Tor000020@gmail.com>
2017-04-05 05:42:44 -0400

    update

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+
+window.onload = initate();
+
+
+	function initate() {
+	    var style1 = document.getElementById("ssbtn1");
+	    var style2 = document.getElementById("ssbtn2");
+	    var style3 = document.getElementById('ssbtn3');
+
+	    style1.addEventListener("click", function() {
+	     swapStyleSheet("styles/default.css");
+	 	})
+	    style2.addEventListener("click", function() {
+	    	swapStyleSheet("styles/dark.css");
+	    })
+	    style3.addEventListener("click", function() {
+	    	swapStyleSheet("styles/display.css");
+	    })
+	}
+
+
+	function swapStyleSheet(sheet){
+		document.getElementById('pagestyle').setAttribute('href', sheet);
+	}
+
